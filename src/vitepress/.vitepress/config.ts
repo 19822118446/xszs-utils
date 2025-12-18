0355e37daf91e13dc25924da1955b069794dddcc
xszs12138 <2457331233@qq.com>
2025-12-18 22:36:52 +0800

    chore:添加自动化vitepress文档

--- src/vitepress/.vitepress/config.ts
+++ src/vitepress/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/xszs-utils/',
   title: 'some tools',
   description: 'A VitePress Site',
   themeConfig: {
